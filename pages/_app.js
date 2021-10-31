bbbcd39e7d57b433637a56bf4b8fa9fea1481e46
Luigi Sanchez <luigi.prodesign@gmail.com>
2021-10-31 17:03:31 +0000

    styled components config

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,30 @@
-import '../styles/globals.css'
+import { createGlobalStyle, ThemeProvider } from "styled-components";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const GlobalStyle = createGlobalStyle`
+  *, *::after, *::before {
+    box-sizing: border-box;
+
+  }
+  body {
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+const theme = {
+  colors: {
+    accent: "#34a29d",
+    primary: "#163d8d",
+  },
+};
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
-
-export default MyApp
